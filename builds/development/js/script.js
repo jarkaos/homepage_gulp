af2f12869c7c004a6fa55bc18db68599a0917cc8
Americo <americo.perez@icloud.com>
2018-10-22 23:28:29 +0200

    Almost finished

--- builds/development/js/script.js
+++ builds/development/js/script.js
@@ -63,4 +63,42 @@ $(window).scroll(function() {
            }, 150 * (i+1)); 
         });
     }
+
+    // thumbnail animations
+    let thumbnail = $('.thumbnail');
+    thumbnail.each(function(i) {
+        if(wScroll > thumbnail.eq(i).offset().top - ($(window).height() / 1.3)) {
+            setTimeout(function() {
+                $('.thumbnail').eq(i).addClass('showing');
+            }, 200 * (i+1));
+        }
+    });
+});
+
+$(function() {
+    // browser window scroll (in pixels) after which the "back to top" link is shown
+    var offset = 300,
+        //browser window scroll (in pixels) after which the "back to top" link opacity is reduced
+        offset_opacity = 1200,
+        //duration of the top scrolling animation (in ms)
+        scroll_top_duration = 700,
+        //grab the "back to top" link
+        $back_to_top = $('.cd-top');
+
+    //hide or show the "back to top" link
+    $(window).scroll(function(){
+        ( $(this).scrollTop() > offset ) ? $back_to_top.addClass('cd-is-visible') : $back_to_top.removeClass('cd-is-visible cd-fade-out');
+        if( $(this).scrollTop() > offset_opacity ) {
+            $back_to_top.addClass('cd-fade-out');
+        }
+    });
+
+    //smooth scroll to top
+    $back_to_top.on('click', function(event){
+        event.preventDefault();
+        $('body,html').animate({
+                scrollTop: 0 ,
+            }, scroll_top_duration
+        );
+    });
 });
